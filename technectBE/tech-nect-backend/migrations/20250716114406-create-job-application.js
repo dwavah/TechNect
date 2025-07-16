6fbd0b0a1cd972d4e96b1d791ab493c714fda555
Daniel Wavamunno <danl.wavah@gmail.com>
2025-07-16 14:48:41 +0300

    preparation

---
+++ technectBE/tech-nect-backend/migrations/20250716114406-create-job-application.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('JobApplications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      studentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      jobId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Jobs',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      status: {
+        type: Sequelize.STRING,
+        defaultValue: 'pending',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('JobApplications');
+  },
+};
